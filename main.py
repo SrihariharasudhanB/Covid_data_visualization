dd3276d123702062f44c77cb407512728f338d60
Srihariharasudhan Balakannan <74477462+SrihariharasudhanB@users.noreply.github.com>
2022-07-10 11:03:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 import requests
 import pandas as pd
 from load import Loader
+from keep_alive import keep_alive
+import time
 
-req = requests.get('https://covid-19-report-api.vercel.app/api/v1/cases/latest')
-data  = req.json()
+URL = 'https://covid-19-report-api.vercel.app/api/v1/cases/latest'
 
-country = []
-confirmed = []
-deaths = []
+def Main():
+    req = requests.get(URL)
+    data  = req.json()
+    
+    country = []
+    confirmed = []
+    deaths = []
+    
+    for datum in data['data']:
+        if 'countryregion' in datum and 'confirmed' in datum and 'deaths' in datum:
+            country.append(datum['countryregion'])
+            confirmed.append(datum['confirmed'])
+            deaths.append(datum['deaths'])
+    
+    data_dict = {
+        'country': country,
+        'confirmed': confirmed,
+        'deaths': deaths
+    }
+    
+    df = pd.DataFrame(data_dict)
+    df = df.groupby('country').sum()
+    df.reset_index(inplace = True)
+    
+    loader = Loader()
+    loader.createTable()
+    loader.deleteData()
+    loader.loadData(df)
+    print('Data refreshed!')
 
-for datum in data['data']:
-    if 'countryregion' in datum and 'confirmed' in datum and 'deaths' in datum:
-        country.append(datum['countryregion'])
-        confirmed.append(datum['confirmed'])
-        deaths.append(datum['deaths'])
-
-data_dict = {
-    "country": country,
-    "confirmed": confirmed,
-    "deaths": deaths
-}
-
-df = pd.DataFrame(data_dict)
-df = df.groupby('country').sum()
-df.reset_index(inplace = True)
-
-loader = Loader()
-loader.createTable()
-loader.deleteData()
-loader.loadData(df)
-print('Data refreshed!')
+if __name__ == '__main__':
+    keep_alive()
+    while True:
+        Main()
+        time.sleep(43200)
+        keep_alive()
